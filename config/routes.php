6045bbd488a106e7701ee1a45d5f0c2ed48f9256
Alex Price <awprice@gmail.com>
2015-08-13 14:49:47 +1000

    Remove test route

--- config/routes.php
+++ config/routes.php
@@ -4,10 +4,6 @@
 		require __DIR__ . '/../controllers/index.php';
 	});
 
-	$router->map('GET', '/hello/world', function() {
-		echo 'This is is a test';
-	});
-
 	$router->map('GET', '/compile/less', function() {
 		require __DIR__ . '/less.compile.php';
 	})
