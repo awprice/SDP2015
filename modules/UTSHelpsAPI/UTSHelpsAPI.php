168e109c40a8f4de92d449bc708bd5e1634f80ff
Alex Price <awprice@gmail.com>
2015-08-17 23:52:00 +1000

    Add ListWorkshopSets endpoint

--- modules/UTSHelpsAPI/UTSHelpsAPI.php
+++ modules/UTSHelpsAPI/UTSHelpsAPI.php
@@ -3,15 +3,32 @@
 	require __DIR__ . '/curl.helper.php';
 
 	/*
-	 * Name: Register Student
-     * Description: Create a student entry in HELPS
-     * Path: api/student/register
-     * Method: POST
-     * Returns: JSON Object
-     * Requires AppKey: true
+	 * Name: RegisterStudent
+	 * Description: Create a student entry in HELPS
+	 * Path: api/student/register
+	 * Method: POST
+	 * Returns: JSON Object
+	 * Requires AppKey: true
 	 */
 	function RegisterStudent($data) {
 		return json_decode(curl_post('api/student/register', $data, true));
+	}
+
+	/*
+	 * Name: ListWorkshopSets
+	 * Description: List the sets of workshops
+	 * Path: api/workshop/workshopSets/{active}
+	 * Method: GET
+	 * Returns: JSON Object
+	 * Requires AppKey: true
+	 */
+	function ListWorkshopSets($active = false) {
+		if ($active) {
+			$active = 'true';
+		} else {
+			$active = 'false';
+		}
+		return json_decode(curl_get('api/workshop/workshopSets/' . $active, true));
 	}
 
 ?>
