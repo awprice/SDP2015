281e5351c0d9a50afae1437e9d306d630603fe55
Alex Price <awprice@gmail.com>
2015-08-18 18:08:32 +1000

    Add put method, general code clean up

--- modules/UTSHelpsAPI/curl.helper.php
+++ modules/UTSHelpsAPI/curl.helper.php
@@ -4,10 +4,9 @@
 
 		$url = $GLOBALS["helps-url"] . $path;
 
-		$ch = curl_init(); 
+		$ch = curl_init($url);
 
 		// Set options
-		curl_setopt($ch, CURLOPT_URL, $url); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 		if ($appkey) {
@@ -26,20 +25,11 @@
 
 		$url = $GLOBALS["helps-url"] . $path;
 
-		$fieldstring = "";
-
-		// Make our post field
-		foreach($data as $key => $value) {
-			$fieldstring .= $key . '=' . urlencode($value) . '&';
-		}
-		$fieldstring = rtrim($fieldstring, '&');
-
-		$ch = curl_init();
+		$ch = curl_init($url);
 
 		// Set options
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, count($data));
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+        curl_setopt($ch, CURLOPT_POST, count($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 		if ($appkey) {
@@ -55,6 +45,26 @@
 	}
 
 	function curl_put($path, $data, $appkey) {
+
+        $url = $GLOBALS['helps-url'] . $path;
+
+        $ch = curl_init($url);
+
+        // Set options
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+        if ($appkey) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
+        }
+
+        $result = curl_exec($ch);
+
+        // close connection
+        curl_close($ch);
+
+        return $result;
 
 	}
 
