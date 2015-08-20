87ad8b95e800ef6417b9b3a69ef4ea7654940884
Alex Price <awprice@gmail.com>
2015-08-20 17:29:20 +1000

    Remove serving of static file functionality as it is too slow

--- router.php
+++ router.php
@@ -7,20 +7,16 @@
 
 	// Import our config files
 	require __DIR__ . '/config/config.php';
-	require __DIR__ . '/config/content_types.php';
 
 	$router = new AltoRouter();
 	require __DIR__ . '/config/routes.php';
 
 	$match = $router->match();
 
-	$extension = pathinfo($_SERVER["REQUEST_URI"])["extension"];
+    header("X-Processed-By: AltoRouter");
 
 	if ($match && is_callable($match['target'])) {
 		call_user_func_array($match['target'], $match['params']);
-	} else if (is_file(__DIR__ . $_SERVER["REQUEST_URI"]) && array_key_exists($extension, $content_types)) {
-		header('Content-Type: ' . $content_types[$extension]);
-		readfile(__DIR__ . $_SERVER["REQUEST_URI"]);
 	} else {
 		http_response_code(404);
 		echo '404 Not Found';
