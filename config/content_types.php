b0042a7e1c0cc4ca5ad6c115a9599c1eef60fd77
Alex Price <awprice@gmail.com>
2015-08-26 14:53:22 +1000

    Move fonts and add appropriate content types

--- config/content_types.php
+++ config/content_types.php
@@ -7,6 +7,11 @@
         'jpg' => 'image/jpeg',
         'css' => 'text/css',
         'html' => 'text/html',
+        'woff' => 'application/x-font-woff',
+        'woff2' => 'application/x-font-woff',
+        'ttf' => 'application/x-font-ttf',
+        'eot' => 'application/vnd.ms-fontobject',
+        'svg' => 'image/svg+xml',
     );
 
 ?>
