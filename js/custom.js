ff5531bfb12ff99402be2a269de701514b621bf1
Alex Price <awprice@gmail.com>
2015-08-27 12:13:10 +1000

    Add numeric plugin to disable letters in number fields

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,29 @@
 (function($) {
 
-  $(document).ready(function() {
+    $(document).ready(function() {
 
-    console.log("Page loaded.");
+        console.log("Page loaded.");
 
-    // Compile less and reload styles every 2 seconds
-    //setInterval(compileLess, 5000);
+        // Restrict all inputs with class .numeric to only enter numbers
+        $('input.numeric').numeric();
 
-  });
 
-  function compileLess() {
-    // Compiling less
-    console.log('Recompiling less');
-    xmlhttp = new XMLHttpRequest();
+        // Compile less and reload styles every 2 seconds
+        //setInterval(compileLess, 5000);
 
-    xmlhttp.open("GET", "/compile/less", false);
-    xmlhttp.send();
+    });
 
-    // Reload css
-    console.log('Reloading css');
-    document.styleSheets.reload();
-  }
+    function compileLess() {
+        // Compiling less
+        console.log('Recompiling less');
+        xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.open("GET", "/compile/less", false);
+        xmlhttp.send();
+
+        // Reload css
+        console.log('Reloading css');
+        document.styleSheets.reload();
+    }
 
 })(jQuery);
