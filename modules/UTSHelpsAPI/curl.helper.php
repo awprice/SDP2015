f4a38cdda381c79ad7aeadec336b68cc1ab9c94f
Alex Price <awprice@gmail.com>
2015-09-03 11:25:44 +1000

    Clean up

--- modules/UTSHelpsAPI/curl.helper.php
+++ modules/UTSHelpsAPI/curl.helper.php
@@ -1,75 +1,99 @@
 <?php
 
-	function curl_get($path, $parameters = null, $appkey) {
+/**
+ * Sends a HTTP Get request
+ *
+ * @param $path
+ * @param null $parameters
+ * @param $appkey
+ * @return mixed
+ */
+function curl_get($path, $parameters = null, $appkey) {
 
-		$url = $GLOBALS["helps-url"] . $path;
+	$url = $GLOBALS["helps-url"] . $path;
 
-        if ($parameters != null) {
-            $url .= http_build_query($parameters);
-        }
+    if ($parameters != null) {
+        $url .= http_build_query($parameters);
+    }
 
-		$ch = curl_init($url);
+	$ch = curl_init($url);
 
-		// Set options
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// Set options
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		if ($appkey) {
-			curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
-		}
-
-		$result = curl_exec($ch); 
-
-		// close connection
-		curl_close($ch);
-
-		return $result;
+	if ($appkey) {
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
 	}
 
-	function curl_post($path, $data, $appkey) {
+	$result = curl_exec($ch);
 
-		$url = $GLOBALS["helps-url"] . $path;
+	// close connection
+	curl_close($ch);
 
-		$ch = curl_init($url);
+	return $result;
+}
 
-		// Set options
-        curl_setopt($ch, CURLOPT_POST, count($data));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+/**
+ * Sends a HTTP Post request
+ *
+ * @param $path
+ * @param $data
+ * @param $appkey
+ * @return mixed
+ */
+function curl_post($path, $data, $appkey) {
 
-		if ($appkey) {
-			curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
-		}
+	$url = $GLOBALS["helps-url"] . $path;
 
-		$result = curl_exec($ch);
+	$ch = curl_init($url);
 
-		// close connection
-		curl_close($ch);
+	// Set options
+    curl_setopt($ch, CURLOPT_POST, count($data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		return $result;
+	if ($appkey) {
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
 	}
 
-	function curl_put($path, $data, $appkey) {
+	$result = curl_exec($ch);
 
-        $url = $GLOBALS['helps-url'] . $path;
+	// close connection
+	curl_close($ch);
 
-        $ch = curl_init($url);
+	return $result;
+}
 
-        // Set options
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+/**
+ * Sends a HTTP Put request
+ *
+ * @param $path
+ * @param $data
+ * @param $appkey
+ * @return mixed
+ */
+function curl_put($path, $data, $appkey) {
 
-        if ($appkey) {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
-        }
+    $url = $GLOBALS['helps-url'] . $path;
 
-        $result = curl_exec($ch);
+    $ch = curl_init($url);
 
-        // close connection
-        curl_close($ch);
+    // Set options
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 
-        return $result;
+    if ($appkey) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['AppKey: ' . $GLOBALS["helps-appkey"]]);
+    }
 
-	}
+    $result = curl_exec($ch);
+
+    // close connection
+    curl_close($ch);
+
+    return $result;
+
+}
 
 ?>
