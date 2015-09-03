3a4e2c89b81fecaba88ec5e7781f1fcadc09e507
Alex Price <awprice@gmail.com>
2015-09-03 11:35:17 +1000

    Add mysql details to config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,13 @@
 <?php
 
-	date_default_timezone_set("Australia/Sydney");
+    date_default_timezone_set("Australia/Sydney");
+
+    $GLOBALS['websiteTitle'] = 'UTS:Helps Mobile';
+
+    $GLOBALS['mysql_host'] = '';
+    $GLOBALS['mysql_database'] = '';
+    $GLOBALS['mysql_user'] = '';
+    $GLOBALS['mysql_pass'] = '';
+    $GLOBALS['mysql_sqlite'] = false;
 
 ?>
