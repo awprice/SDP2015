95f6a669304c9163b8211a45f2d8b61c6424a834
Alex Price <awprice@gmail.com>
2015-10-20 09:00:36 +1100

    Fix error with index loading slowly

--- controllers/index.php
+++ controllers/index.php
@@ -10,27 +10,28 @@ if (User::isLoggedIn()) {
         Session::redirect('/register');
     }
     $page['loggedin'] = true;
-}
 
-$bookings = UTSHelpsAPI::SearchWorkshopBookings([
-    'studentId' => User::getPaddedId(),
-    'pageSize' => 9999,
-    'active' => true,
-]);
+    $bookings = UTSHelpsAPI::SearchWorkshopBookings([
+        'studentId' => User::getPaddedId(),
+        'pageSize' => 9999,
+        'active' => true,
+    ]);
 
-$count = 0;
+    $count = 0;
 
-if ($bookings != null && $bookings->IsSuccess == 1) {
-    foreach ($bookings->Results as $booking) {
-        // if booking archived field does not have a date
-        $startDate = strtotime($booking->starting);
-        if ($booking->BookingArchived == null && $startDate > $currentTime) {
-            $count++;
+    if ($bookings != null && $bookings->IsSuccess == 1) {
+        foreach ($bookings->Results as $booking) {
+            // if booking archived field does not have a date
+            $startDate = strtotime($booking->starting);
+            if ($booking->BookingArchived == null && $startDate > $currentTime) {
+                $count++;
+            }
         }
     }
-}
 
-$page['bookingCount'] = $count;
+    $page['bookingCount'] = $count;
+
+}
 
 
 ?>
