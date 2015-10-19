d985b70f0243c5d90f93ef4dcce798ee5784da8d
Alex Price <awprice@gmail.com>
2015-10-20 09:04:21 +1100

    Clean up index code

--- controllers/index.php
+++ controllers/index.php
@@ -2,28 +2,31 @@
 
 $page['loggedin'] = false;
 
-$currentTime = strtotime(Session::getCurrentDateTime());
-
 if (User::isLoggedIn()) {
+
+    // If it's the user's first use, make sure they register
     if (User::firstUse()) {
         Session::setError('You have to register first before using this service.');
         Session::redirect('/register');
     }
+
     $page['loggedin'] = true;
 
+    // search for the users bookings
     $bookings = UTSHelpsAPI::SearchWorkshopBookings([
         'studentId' => User::getPaddedId(),
         'pageSize' => 9999,
         'active' => true,
     ]);
 
+    $currentTime = strtotime(Session::getCurrentDateTime());
     $count = 0;
 
+    // Get all the upcoming bookings
     if ($bookings != null && $bookings->IsSuccess == 1) {
         foreach ($bookings->Results as $booking) {
             // if booking archived field does not have a date
-            $startDate = strtotime($booking->starting);
-            if ($booking->BookingArchived == null && $startDate > $currentTime) {
+            if ($booking->BookingArchived == null && strtotime($booking->starting) > $currentTime) {
                 $count++;
             }
         }
