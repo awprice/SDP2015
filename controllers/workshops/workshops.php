9c47ed2e85f3103d80d7e529670424b7a1db2910
Alex Price <awprice@gmail.com>
2015-11-02 22:15:51 +1100

    Sort workshop sets by name

--- controllers/workshops/workshops.php
+++ controllers/workshops/workshops.php
@@ -1,9 +1,25 @@
 <?php
 
+/**
+ * @var $page
+ */
+
+/**
+ * Compare two workshop names
+ *
+ * @param $a
+ * @param $b
+ * @return int
+ */
+function compareByName($a, $b) {
+    return strcmp($a->name, $b->name);
+}
+
 $workshops = UTSHelpsAPI::ListWorkshopSets(true);
 
 if ($workshops != null && $workshops->IsSuccess == 1) {
-    $page['workshops'] = $workshops;
+    $page['workshops'] = $workshops->Results;
+    usort($page['workshops'], 'compareByName');
 } else {
     $page['workshops'] = null;
 }
