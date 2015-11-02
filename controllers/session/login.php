a7ce6e6343e50d7f1835ab10f16d15dd104f5a39
Alex Price <awprice@gmail.com>
2015-11-02 22:37:46 +1100

    Re enable 1 account

--- controllers/session/login.php
+++ controllers/session/login.php
@@ -17,7 +17,6 @@
             if (in_array($_POST['login']['studentId'], [
                 '11111111',
                 '22222222',
-                '33333333',
             ])) {
                 Session::setError('Your Student ID or Password was incorrect or the account does not exist, please try again.');
                 Session::redirect('/login');
